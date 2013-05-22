9e83b89c0f1d57d0866c7a1e998e9a388d28f038
Aitor Mendaza <aitorTheRed@gmail.com>
2013-05-22 12:25:08 +0200

    Mas test, despues del repaso que nos ha dado leo

--- src/scrumcourse/fullcycle/service/ServiceUsuario.java
+++ src/scrumcourse/fullcycle/service/ServiceUsuario.java
@@ -1,6 +1,19 @@
 package scrumcourse.fullcycle.service;
 
+import scrumcourse.fullcycle.dao.UserDao;
+import scrumcourse.fullcycle.entity.User;
+import scrumcourse.fullcycle.exception.DaoException;
+
 public class ServiceUsuario {
+	UserDao myUserDao;
+
+	// public ServiceUsuario(UserDao _myUserDao) {
+	// this.myUserDao = _myUserDao;
+	// }
+
+	public void setUserDao(UserDao _myUserDao) {
+		this.myUserDao = _myUserDao;
+	}
 
 	public boolean canLogin(String user, String password) {
 		throw new RuntimeException("NOT IMPLEMENTED YET ServiceLogin.canLogin");
@@ -8,8 +21,19 @@ public class ServiceUsuario {
 
 	public boolean insertUser(String userId, String nombre, String correo,
 			String password) {
-		throw new RuntimeException(
-				"NOT IMPLEMENTED YET ServicePersistence.insertUser");
+		User myUser = new User();
+		if (nombre.equals("") || userId.equals("") || correo.equals("")) {
+			return false;
+		}
+		myUser.setName(nombre);
+		myUser.setEmail(correo);
+		myUser.setUserId(userId);
+		try {
+			myUserDao.insert(myUser);
+		} catch (DaoException daoe) {
+			return false;
+		}
+		return true;
 	}
 
 }
--- test/unit/scrumcourse/fullcycle/service/TestServiceUsuario.java
+++ test/unit/scrumcourse/fullcycle/service/TestServiceUsuario.java
@@ -1,25 +1,72 @@
 package unit.scrumcourse.fullcycle.service;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 
+import scrumcourse.fullcycle.dao.UserDao;
+import scrumcourse.fullcycle.entity.User;
+import scrumcourse.fullcycle.exception.DaoException;
+import scrumcourse.fullcycle.service.ServiceUsuario;
+
 public class TestServiceUsuario {
 
-	@Test
-	public void testCanLogin() {
-		fail("Not yet implemented");
-	}
+	// @Test
+	// public void testCanLogin() {
+	// fail("Not yet implemented");
+	// }
 
 	/**
-	 * Requisitos: 
-	 * - Si inserto algo vacio, devuelve false 
-	 * - Si inserto un usuario no existente, que devuelva true
-	 * - Si inserto un user que ya existe(), que devuelva false
+	 * Requisitos de inser test: + Si inserto algún campo *required(UserId,
+	 * nombre, mail, password) vacio, devuelve false - Si inserto un usuario no
+	 * existente, que devuelva true - Si inserto un user que ya existe(userId),
+	 * que devuelva false
 	 */
 	@Test
-	public void testInsertUser() {
-		fail("Not yet implemented");
+	public void testInserUserWithSomeFieldEmpty() {
+		ServiceUsuario su = new ServiceUsuario();
+		assertFalse(su.insertUser("", "Nombre", "mail@test.com", "password"));
+		assertFalse(su.insertUser("userId", "", "mail@test.com", "password"));
+		assertFalse(su.insertUser("userId", "Nombre", "", "password"));
+	}
+
+	@Test
+	public void testWhenNonExistingUserCallInsertDaoReturnsTrue() {
+		UserDao mockedUserDao = mock(UserDao.class);
+		ServiceUsuario su = new ServiceUsuario();
+		su.setUserDao(mockedUserDao);
+		assertTrue(su.insertUser("userId", "Nombre", "mail@test.com",
+				"password"));
+	}
+
+	@Test
+	public void testWhenNonExistingUserCallInsertDao() {
+		UserDao mockedUserDao = mock(UserDao.class);
+
+		ServiceUsuario su = new ServiceUsuario();
+		su.setUserDao(mockedUserDao);
+		assertTrue(su.insertUser("userId", "Nombre", "mail@test.com",
+				"password"));
+		verify(mockedUserDao, times(1)).insert((User) any());
+	}
+
+	@Test
+	public void testInsertExistingUserWithDao() {
+		UserDao mockedUserDao = mock(UserDao.class);
+		doThrow(new DaoException()).when(mockedUserDao).insert((User) any());
+		ServiceUsuario su = new ServiceUsuario();
+		su.setUserDao(mockedUserDao);
+
+		assertEquals(false,
+				su.insertUser("userId", "Nombre", "mail@test.com", "password"));
+		verify(mockedUserDao).insert((User) any());
 	}
 
 }

557b598e9526c20ee59369ae001e85aeafc83716
Aitor Mendaza <aitorTheRed@gmail.com>
2013-05-22 11:37:08 +0200

    Added mockito jar version 1.9.5

---
+++ test/unit/scrumcourse/fullcycle/service/TestServiceUsuario.java
@@ -0,0 +1,25 @@
+package unit.scrumcourse.fullcycle.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestServiceUsuario {
+
+	@Test
+	public void testCanLogin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Requisitos: 
+	 * - Si inserto algo vacio, devuelve false 
+	 * - Si inserto un usuario no existente, que devuelva true
+	 * - Si inserto un user que ya existe(), que devuelva false
+	 */
+	@Test
+	public void testInsertUser() {
+		fail("Not yet implemented");
+	}
+
+}
